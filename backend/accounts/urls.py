e7aa58e14923b90036d41541dc4830483dab83d4
AllenAbrahamThomas <allenabraham194@gmail.com>
2026-06-18 16:04:54 +0530

    feat(backend/accounts): implement accounts models, views, and migrations

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,24 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from .views import LoginView, recent_logins
+from .views import (
+    LoginView,
+    recent_logins,
+    AccountHeadViewSet,
+    CashTransactionViewSet,
+    JournalEntryViewSet,
+    daybook_view,
+)
 
+
+router = DefaultRouter()
+router.register("heads", AccountHeadViewSet, basename="account-head")
+router.register("transactions", CashTransactionViewSet, basename="cash-transaction")
+router.register("journals", JournalEntryViewSet, basename="journal")
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("login-sessions/", recent_logins, name="login-sessions"),
+    path("daybook/", daybook_view, name="daybook"),
+    path("", include(router.urls)),
 ]
